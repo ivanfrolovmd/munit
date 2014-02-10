c43c83fb8c0934b3747b8b67cae64e22cc294e9f
Damian Pelaez <damian.pelaez@mulesoft.com>
2014-02-10 10:59:33 -0300

    Fixed licenses and javadocs

--- munit-maven-tools/munit-maven-plugin/src/main/java/org/mule/properties/MUnitUserPropertiesManager.java
+++ munit-maven-tools/munit-maven-plugin/src/main/java/org/mule/properties/MUnitUserPropertiesManager.java
@@ -6,22 +6,6 @@
  */
 package org.mule.properties;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -32,8 +16,6 @@ import java.util.Set;
  * {@link MUnitUserPropertiesManager} allows to set the system properties
  * defined by the user for the execution of the MUnit tests. It also allows
  * to save and restore the state of the system properties on call.
- * 
- * @author damianpelaez
  */
 public class MUnitUserPropertiesManager
 {
